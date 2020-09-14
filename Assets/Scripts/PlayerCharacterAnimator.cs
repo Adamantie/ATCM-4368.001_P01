2e32058ad63255f8520aa4fbe0b4cad5fb70dfa7
Carlos Sandoval <cdsandoval998@gmail.com>
2020-09-14 17:55:43 -0500

    Finished character controller setup doc

---
+++ Assets/Scripts/PlayerCharacterAnimator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerCharacterAnimator : MonoBehaviour
+{
+    [SerializeField] ThirdPersonMovement _thirdPersonMovement = null;
+    
+    // these names align with the naming in our Animator node
+    const string IdleState = "Idle";
+    const string RunState = "Run";
+    const string JumpState = "Jumping";
+    const string FallState = "Falling";
+
+    Animator _animator = null;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void OnIdle()
+    {
+        _animator.CrossFadeInFixedTime(IdleState, .2f);
+    }
+
+    private void OnStartRunning()
+    {
+        _animator.CrossFadeInFixedTime(RunState, .2f);
+    }
+
+    private void OnEnable()
+    {
+        _thirdPersonMovement.Idle += OnIdle;
+        _thirdPersonMovement.StartRunning += OnStartRunning;
+    }
+
+    private void OnDisable()
+    {
+        _thirdPersonMovement.Idle -= OnIdle;
+        _thirdPersonMovement.StartRunning -= OnStartRunning;
+    }
+}
--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
+    public event Action Idle = delegate { };
+    public event Action StartRunning = delegate { };
+
+    [SerializeField] CharacterController _controller = null;
+    [SerializeField] Transform _cam = null;
+
     public CharacterController controller;
     public Transform cam;
 
@@ -11,15 +18,24 @@ public class ThirdPersonMovement : MonoBehaviour
     public float turnSmoothTime = 0.1f;
 
     float turnSmoothVelocity;
+    bool _isMoving = false;
 
-    void Update()
+    private void Start()
+    {
+        Idle?.Invoke();
+    }
+
+    private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
+            CheckIfStartedMoving();
+
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
@@ -27,5 +43,33 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+        else
+        {
+            CheckIfStoppedMoving();
+        }
+    }
+
+    private void CheckIfStartedMoving()
+    {
+        if (_isMoving == false)
+        {
+            // our velocity says we're moving but we previously were not
+            // this means we've started moving!
+            StartRunning?.Invoke();
+            Debug.Log("Started");
+        }
+        _isMoving = true;
+    }
+
+    private void CheckIfStoppedMoving()
+    {
+        if (_isMoving == true)
+        {
+            // our velocity says we're not moving but we previously were
+            // this means we've stopped!
+            Idle?.Invoke();
+            Debug.Log("Stopped");
+        }
+        _isMoving = false;
     }
 }
